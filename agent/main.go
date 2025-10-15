fb58110d93ee4ac244904434b1dd25aee43f7504
Sid-Romero <sidya.b18@gmail.com>
2025-10-16 01:33:39 +0200

    feat: stable K8s manifests with Secrets and retention cronjob

--- agent/main.go
+++ agent/main.go
@@ -12,7 +12,6 @@ import (
 	"github.com/shirou/gopsutil/v3/mem"
 )
 
-
 type Metric struct {
 	Hostname  string  `json:"hostname"`
 	CPU       float64 `json:"cpu"`
@@ -21,13 +20,8 @@ type Metric struct {
 }
 
 func collectMetrics() Metric {
-	// Hostname
 	hostname, _ := os.Hostname()
-
-	// CPU usage (percentage)
 	cpuPercent, _ := cpu.Percent(time.Second, false)
-
-	// Memory usage
 	vmStat, _ := mem.VirtualMemory()
 
 	return Metric{
@@ -38,23 +32,28 @@ func collectMetrics() Metric {
 	}
 }
 
-func sendMetrics(m Metric) {
+func sendMetrics(m Metric, backendURL string) {
 	data, _ := json.Marshal(m)
 
-	resp, err := http.Post("http://localhost:8080/metrics", "application/json", bytes.NewBuffer(data))
+	resp, err := http.Post(backendURL, "application/json", bytes.NewBuffer(data))
 	if err != nil {
 		log.Println("❌ Error sending metrics:", err)
 		return
 	}
 	defer resp.Body.Close()
 
-	log.Printf("Sent metrics: Host=%s CPU=%.2f%% Mem=%.2f%%", m.Hostname, m.CPU, m.Memory)
+	log.Printf("✅ Sent metrics: Host=%s CPU=%.2f%% Mem=%.2f%%", m.Hostname, m.CPU, m.Memory)
 }
 
 func main() {
+	backendURL := os.Getenv("BACKEND_URL")
+	if backendURL == "" {
+		log.Fatal("❌ BACKEND_URL environment variable is not set")
+	}
+
 	for {
 		metric := collectMetrics()
-		sendMetrics(metric)
-		time.Sleep(5 * time.Second) // every 5 seconds
+		sendMetrics(metric, backendURL)
+		time.Sleep(5 * time.Second)
 	}
 }
